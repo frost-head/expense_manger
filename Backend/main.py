fadd96a14f1c395491247b2559d178175e00c265
frost-head <ed.ayush2003@gmail.com>
2022-03-04 21:51:49 +0530

    Removed Api functionality

--- Backend/main.py
+++ Backend/main.py
@@ -1,47 +1,26 @@
 # imports
 from flask import Flask, json
 import os
-from flask_mysqldb import MySQL
-from database import *
-from flask_restful import Resource, Api, reqparse
+# from flask_mysqldb import MySQL
+# from database import *
 
 
 # app setup
 app = Flask(__name__)
-mysql = MySQL(app)
-api = Api(app)
+# mysql = MySQL(app)
 
 
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = os.environ.get("Mysql_user")
-app.config['MYSQL_PASSWORD'] = os.environ.get("Mysql_pass")
-app.config['MYSQL_DB'] = 'expense_manager'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = os.environ.get("Mysql_user")
+# app.config['MYSQL_PASSWORD'] = os.environ.get("Mysql_pass")
+# app.config['MYSQL_DB'] = 'expense_manager'
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.secret_key = os.urandom(24)
 
 
-
-user_parser = reqparse.RequestParser()
-user_parser.add_argument('Name', type=str, help='Name of user')
-user_parser.add_argument('Username', type=str, help='Username for user')
-user_parser.add_argument('Password', type=str, help='Password for user')
-
-
-
-# classes
-class users(Resource):
-    def post(self):
-        args = user_parser.parse_args()
-        print(args)
-        insert(mysql,f"insert into Users(name, username, password) values('{args['Name']}', '{args['Username']}', '{args['Password']}')")
-        return {'operation':"success"}
-    
-    def get(self):
-        return fetchall(mysql, "select * from Users")
-
-
-api.add_resource(users, '/createuser')
+@app.route("/")
+def Home():
+    return "<h1>API functionality Removed</h1>"
 
 app.run(debug=True, host="0.0.0.0")
--- Backend/test.py
+++
@@ -1,9 +0,0 @@
-#to test the api
-
-from requests import post, get
-
-post('http://192.168.43.192:5000/createuser', data={'Name':'Astha', 'Username':'MySunshine','Password':'tes2'} )
-
-data = get('http://192.168.43.192:5000/createuser' ).json()
-
-print(data)
