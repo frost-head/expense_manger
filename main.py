a82ec23eaa3048dca97dabb70e61ff007483c536
frost-head <ed.ayush2003@gmail.com>
2022-03-18 14:55:45 +0530

    account creation and login and logut

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # imports
 from crypt import methods
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session
 import os
 from flask_mysqldb import MySQL
 from database import *
+from flask_bcrypt import Bcrypt
 
 
 # app setup
 app = Flask(__name__)
 mysql = MySQL(app)
+bcrypt = Bcrypt(app)
 
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -26,10 +28,49 @@ def Home():
 
 @app.route("/login",methods=["GET","POST"])
 def Login():
+    if 'user' in session:
+        return redirect('/')
+    if request.method == "POST":
+        username = request.form['username']
+        passcode = request.form['password']
+        data = fetchone(mysql, "select uid, password from Users where username = '{}'".format(username))
+        if data:
+            if bcrypt.check_password_hash(data['password'],passcode):
+                session['user'] = data['uid']
+                return redirect('/')
+            else:
+                return redirect('/login')
+        else:
+            return redirect('/login')
     return render_template("Login.html")
 
 @app.route("/register",methods=["GET","POST"])
 def Register():
+    if 'user' in session:
+        return redirect('/')
+    if request.method == "POST":
+        username = request.form['username']
+        data = fetchone(mysql, "select uid from Users where username = '{}'".format(username))
+        if data:
+            return "username taken"
+        name = request.form['name']
+        email = request.form['email']
+        passcode = request.form['password']
+        pw_hash = bcrypt.generate_password_hash(passcode).decode('utf-8')
+        querry = 'insert into Users(username, name, email, password) values("{}","{}","{}","{}")'.format(username, name, email, pw_hash)
+        print(querry)
+        insert(mysql, querry)
+        # uid = fetchone(mysql, "select uid from Users where username = '{}'".format(username))
+        # session['user'] = uid
+        return redirect('/')
     return render_template("Register.html")
+
+@app.route("/logout")
+def Logout():
+    if 'user' in session:
+        session.pop('user')
+        return redirect('/')
+    else:
+        return redirect("/login")
 
 app.run(debug=True, host="0.0.0.0")
--- static/animation.js
+++ static/animation.js
@@ -1,7 +1,7 @@
 // navbar
 
 var nav = gsap.timeline({defaults:{duration:0.5}});   
-nav.fromTo("nav", { y:-80 , opacity: 0.2}, {y:0, opacity:1, ease: Circ.easeOut} )
+nav.fromTo("nav", { y:-80 , opacity: 0.2}, {y:0, opacity:1, ease: Circ.easeOut}, "start")
     .fromTo(".navtitle", {y:-50 , opacity: 0.5}, {y:0 , opacity:1, ease: Circ.easeOut},'<.5')
     .fromTo(".navlinks", {y:-50 , opacity: 0.5}, {y:0 , opacity:1, ease: Circ.easeOut},'<')
 ;
@@ -9,7 +9,8 @@ nav.fromTo("nav", { y:-80 , opacity: 0.2}, {y:0, opacity:1, ease: Circ.easeOut} 
 // login page
 
   
-nav.fromTo(".loginForm", {opacity: 0.0, scale:0.8}, {opacity:1, scale:1, ease: Circ.easeOut},"0.5" )
-     .fromTo(".loginForm h2", {y:-80 , opacity: 0.0}, {y:0 , opacity:1, ease: Circ.easeOut},)
-     .fromTo("form", {y:-40 , opacity: 0.0}, {y:0 , opacity:1, ease: Circ.easeOut},"<")
+// nav.fromTo(".loginForm", {opacity: 0.0, scale:0.8}, {opacity:1, scale:1, ease: Circ.easeOut},"start=0.5" )
+nav.fromTo(".loginForm", {opacity: 0.0, y:-100}, {opacity:1, y:0, ease: Circ.easeOut},"start=0.5" )
+     .fromTo(".loginForm h2", {y:-80 , opacity: 0.0}, {y:0 , opacity:1, ease: Circ.easeOut},"<0.5")
+     .fromTo("form", {y:-60 , opacity: 0.0}, {y:0 , opacity:1, ease: Circ.easeOut},"<")
 ;
