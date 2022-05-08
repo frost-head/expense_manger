7d728cd5e867be2e234b4da1da5fac983add2db4
frost-head <ed.ayush2003@gmail.com>
2022-05-08 17:05:23 +0530

    Revert "one graph done with database connectivity"
    This reverts commit d10d622bdbf904933615a2002ec67fbfd96fa1ba.

--- static/charts.js
+++ static/charts.js
@@ -1,46 +1,69 @@
-// let ctx = document.getElementById("incomevsexpensesline")
+Chart.defaults.color = "#FBFEF9";
 
-// const data = {
-//   labels: ['monday', 'tuesday', 'wednesday', 'thursday', 'saturday', 'friday', 'sunday'],
-//   datasets: [{
-//     label: 'My First dataset',
-//     backgroundColor: '#FF4D4D',
-//     borderColor: '#FF4D4D',
-//     data: [0, 10, 5, 50, 20, 30, 45],
-//   }, {
-//     label: 'My First dataset',
-//     backgroundColor: '#6DFF8C',
-//     borderColor: '#6DFF8C',
-//     data: [10, 15, 51, 40, 2, 35, 40],
-//   }]
-// }
 
-// const ivel = new Chart(
-//   ctx,
-//   config = {
-//     type: 'line',
-//     data: data,
-//     options: {
+let ctx = document.getElementById("incomevsexpensesline")
+let ctx1 = document.getElementById("incomevsexpensesbar")
+const data = {
+    labels:['monday', 'tuesday','wednesday','thursday','saturday','friday','sunday'],
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: '#FF4D4D',
+        borderColor: '#FF4D4D',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },{
+        label: 'My First dataset',
+        backgroundColor: '#6DFF8C',
+        borderColor: '#6DFF8C',
+        data: [10, 15, 50, 20, 2, 35, 40],
+      }]
+}
 
-//       hoverRadius: 8,
-//       radius: 5,
-//       hitRadius: 20,
-//       scales: {
-//         x: {
-//           grid: {
-//             color: '#fbfef952',
+const ivel = new Chart(
+    ctx,
+    config = {
+        type: 'line',
+        data: data,
+        options:{
 
-//           }
-//         },
+            
+            scales: {
+                x: {
+                  grid: {
+                    color: '#fbfef952',
 
-//         y: {
-//           grid: {
-//             color: '#fbfef952',
+                  }
+                },
+              
+            y:{
+                grid: {
+                    color: '#fbfef952',
 
-//           }
-//         },
-//       },
-//       responsive: true,
-//     },
-//   }
-// );
+                  }
+            },},
+            responsive:true,
+        },
+    }
+);
+
+const data1 = {
+    labels:['monday', 'tuesday'],
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: ['#6DFF8C','#FF4D4D'],
+        borderColor: ['#6DFF8C','#FF4D4D'],
+        data: [25, 5],
+        
+      }]
+}
+
+const iveb = new Chart(
+    ctx1,
+    config = {
+        type: 'bar',
+        data: data1,
+        options:{
+
+            responsive:true,
+        },
+    }
+);
